839167bea61dbb222fd08e0df44624cb0dae6028
Evgeniy Malyutin <evxn@users.noreply.github.com>
2021-12-06 18:17:39 +0300

    moved index.html to src

--- index.js
+++
@@ -1,5 +0,0 @@
-import '@github/time-elements/dist/local-time-element'
-
-for (const elem of document.querySelectorAll('.year-current')) {
-    elem.setAttribute('datetime', new Date().toISOString())
-}
